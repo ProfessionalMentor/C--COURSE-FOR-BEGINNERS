f75e94ea5781a849a8979faca7b0da64a349cd49
Muhammad Ali <153981195+ProfessionalMentor@users.noreply.github.com>
2024-01-03 15:02:00 +0500

    Add files via upload

---
+++ tut2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a,b,x;
+
+  cout<<"enter value of a and b";
+ 
+  cin>>a>>b;
+
+  x=a-b;
+
+  cout<<x;
+}
---
+++ tut3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+   bool test;
+
+   cout<<"enter value of a and b"<<endl;
+   cin>>a>>b;
+   test=(a==b);
+   cout<<test;
+
+
+   return 0;
+}
