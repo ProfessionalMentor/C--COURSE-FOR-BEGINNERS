cbfa3741a70853844be2691ef220b5e2702ce943
Muhammad Ali <153981195+ProfessionalMentor@users.noreply.github.com>
2024-01-11 23:44:55 +0500

    Add files via upload

---
+++ tut4.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // Conditional Statement
+
+    int a, b, c;
+    cout << "Enter the value a , b , c = ";
+    cin >> a >> b >> c;
+
+    if (a > b || a > c)
+    {
+        cout << " a is greater";
+    }
+}
+
+//         else if (b > a && b > c)
+//     {
+//         cout << " b is greater";
+//     }
+
+//     else
+//     {
+//         cout << "c is greater";
+//     }
+// }
---
+++ tut5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int i ;
+  
+  
+  for ( i = 1; i <= 100; i++)
+  {
+    cout<<"Muhammad Ali"<<endl;
+  }
+  
+  
+ // while (i <= 50)
+
+   return 0;
+}
